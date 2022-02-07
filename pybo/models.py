1a849656e398bceccd3cafe22c8a34188e1630fc
son <ililil11t@naver.com>
2022-02-07 16:23:53 +0900

    django practice

---
+++ pybo/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateField()
+    modify_date = models.DateTimeField(null= True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_question')  # 추천인 추가
+
+    def __str__(self):
+        return self.subject
+
+
+class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)  # ForeignKey 다른모델과 연결하기위해 사용(Question), on_delete는 질문이 삭제되면 답변도 같이 삭제된다는 뜻
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)
+
+
